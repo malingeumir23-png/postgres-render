e7d59b4de8bc5a09b1464385162234a587c43123
malingeumir23-png <maling.eumir23@gmail.com>
2026-05-04 01:50:04 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,7 @@
 from fastapi import FastAPI
-from database import engine, Base
-from models import Character
 
 app = FastAPI()
 
-@app.on_event("startup")
-async def startup_event():
-    Base.metadata.create_all(bind=engine)
-
 @app.get("/")
-async def root():
-    return {"message": "PostgreSQL Connected"}
+def root():
+    return {"status": "ok"}
